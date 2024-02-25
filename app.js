196fbb3a5f285e4c3cbffd380eeff35a85a8df2b
danilo-89 <danilo89abcd@gmail.com>
2024-02-25 01:04:06 +0100

    try to fix rewrite rules for render.com

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const history = require('connect-history-api-fallback')
+
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.use(history())
 
 app.get('/version', (req, res) => {
     res.send('0') // change this string to ensure a new version deployed
